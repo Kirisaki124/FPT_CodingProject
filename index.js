463d3c0013970a54f7dd1c49071c3325cc3127df
omega1100100 <omega1100100@gmail.com>
2018-07-10 21:00:26 +0700

    ok

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const projectRouter = require('./api/projects/router');
-const leaderRouter = require('./api/leaders/router');
-const eventRouter = require('./api/events/router');
+const projectRouter = require("./api/projects/router");
+const leaderRouter = require("./api/leaders/router");
+const eventRouter = require("./api/events/router");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 mongoose.connect(
-  'mongodb://localhost:27017/js-club',
+  "mongodb://localhost:27017/js-club",
   // @ts-ignore
   { useNewUrlParser: true },
   err => {
     if (err) console.log(err);
-    else console.log('Database connected');
+    else console.log("Database connected");
   }
 );
 
-app.use('/api/projects', projectRouter);
-app.use('/api/leaders', leaderRouter);
-app.use('/api/events', eventRouter);
+app.use("/api/projects", projectRouter);
+app.use("/api/leaders", leaderRouter);
+app.use("/api/events", eventRouter);
 
 const port = process.env.PORT || 6969;
 
 app.listen(port, err => {
   if (err) console.log(err);
-  console.log('Server start at ' + port);
+  console.log("Server start at " + port);
 });
